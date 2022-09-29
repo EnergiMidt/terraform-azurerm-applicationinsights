16c9f61c35ec1f090b8bc6a6af602e8df8e745fc
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2022-09-29 13:19:44 +0200

    feature: application insights

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# terraform-azurerm-applicationinsights
+# [terraform-azurerm-applicationinsights](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/application_insights)
+
 Manages an Application Insights component.
+
+## Getting Started
+
+- Format and validate terraform code before commit.
+
+```shell
+terraform init -upgrade \
+    && terraform init -reconfigure -upgrade \
+    && terraform fmt -recursive . \
+    && terraform fmt -check \
+    && terraform validate .
+```
+
+- Always fetch latest changes from upstream and rebase from it. Terraform documentation will always be updated with GitHub Actions. See also [.github/workflows/terraform.yml](.github/workflows/terraform.yml) GitHub Actions workflow.
+
+```shell
+git fetch --all --prune --prune-tags \
+  && git pull --rebase --all --prune --tags
+```
+
+<!-- BEGIN_TF_DOCS -->
+<!-- END_TF_DOCS -->
---
+++ variables.tf
@@ -0,0 +1,145 @@
+variable "name" {
+  description = " (Required) Specifies the name of the Application Insights component. Changing this forces a new resource to be created."
+  type        = string
+}
+
+variable "override_name" {
+  description = "(Optional) Override the name of the resource. Under normal circumstances, it should not be used."
+  type        = string
+  default     = ""
+}
+
+variable "environment" {
+  description = "(Required) The name of the environment."
+  type        = string
+  validation {
+    condition = contains([
+      "dev",
+      "test",
+      "prod",
+    ], var.environment)
+    error_message = "Valid options are dev, test, and prod."
+  }
+}
+
+variable "resource_group" {
+  description = "(Required) The resource group in which to create the Application Insights component."
+  type        = any
+}
+
+# This upstream variable is replaced by the use of `resource_group` variable.
+# variable "resource_group_name" {
+#   description = "(Required) The name of the resource group in which the Log Analytics workspace is created. Changing this forces a new resource to be created."
+#   type        = string
+# }
+
+# This upstream variable is replaced by the use of `resource_group` variable.
+# variable "location" {
+#   description = "(Required) Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
+#   type        = string
+# }
+
+variable "override_location" {
+  description = "(Optional) Specifies whether to override the location of the resource. Usually, it should not be used."
+  default     = ""
+  type        = string
+}
+
+variable "application_type" {
+  default     = "other"
+  description = "(Optional) Specifies the type of Application Insights to create. Valid values are `ios` for iOS, `java` for Java web, `MobileCenter` for App Center, `Node.JS` for Node.js, `other` for General, `phone` for Windows Phone, `store` for Windows Store and `web` for ASP.NET. Please note these values are case sensitive; unmatched values are treated as ASP.NET by Azure. Changing this do not force a new resource to be created."
+  type        = string
+  validation {
+    condition = contains([
+      "ios",
+      "java",
+      "MobileCenter",
+      "Node.JS",
+      "other",
+      "phone",
+      "store",
+      "web",
+    ], var.application_type)
+    error_message = "Valid options are `ios`, `java`, `MobileCenter`, `Node.JS`, `other`, `phone`, `store`, or `web`."
+  }
+}
+
+variable "daily_data_cap_in_gb" {
+  description = "(Optional) Specifies the Application Insights component daily data volume cap in GB."
+  default     = null
+  type        = number
+}
+
+variable "daily_data_cap_notifications_disabled" {
+  description = "(Optional) Specifies if a notification email will be send when the daily data volume cap is met."
+  default     = null
+  type        = bool
+}
+
+variable "retention_in_days" {
+  default     = "90"
+  description = "(Optional) Specifies the retention period in days. Possible values are `30`, `60`, `90`, `120`, `180`, `270`, `365`, `550` or `730`. Defaults to `90`."
+  type        = number
+  validation {
+    condition = contains([
+      30,
+      60,
+      90,
+      120,
+      180,
+      270,
+      365,
+      550,
+      750,
+    ], var.retention_in_days)
+    error_message = "Valid options are `30`, `60`, `90`, `120`, `180`, `270`, `365`, `550` or `730`."
+  }
+}
+
+variable "sampling_percentage" {
+  description = "(Optional) Specifies the percentage of the data produced by the monitored application that is sampled for Application Insights telemetry."
+  default     = "100"
+  type        = number
+}
+
+variable "disable_ip_masking" {
+  description = "(Optional) By default the real client IP is masked as `0.0.0.0` in the logs. Use this argument to disable masking and log the real client IP. Defaults to `false`."
+  default     = false
+  type        = bool
+}
+
+variable "tags" {
+  description = "(Optional) A mapping of tags to assign to the resource."
+  default     = {}
+  type        = map(string)
+}
+
+variable "workspace_id" {
+  description = "(Optional) Specifies the id of a log analytics workspace resource. Changing this forces a new resource to be created."
+  default     = null
+  type        = string
+}
+
+variable "local_authentication_disabled" {
+  description = "(Optional) Disable Non-Azure AD based Auth. Defaults to `false`."
+  default     = false
+  type        = bool
+}
+
+variable "internet_ingestion_enabled" {
+  description = "(Optional) Should the Application Insights component support ingestion over the Public Internet? Defaults to `true`."
+  default     = true
+  type        = bool
+}
+
+variable "internet_query_enabled" {
+  description = "(Optional) Should the Application Insights component support querying over the Public Internet? Defaults to `true`."
+  default     = true
+  type        = bool
+}
+
+variable "force_customer_storage_for_profiler" {
+  description = "(Optional) Should the Application Insights component force users to create their own storage account for profiling? Defaults to `false`."
+  default     = false
+  type        = bool
+}
