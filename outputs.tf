9559e92b9a0fab9c30678a4b627da099b8e60feb
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2022-10-03 12:56:19 +0200

    docs: update description

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "azurerm_application_insights" {
-  description = "The Application Insights resource."
+  description = "The Azure Application Insights resource."
   value       = azurerm_application_insights.insights
 }
