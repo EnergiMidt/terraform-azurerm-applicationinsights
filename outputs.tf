0e0f68fce82eeedf5431247664847d45d79bb089
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2022-10-04 19:47:00 +0200

    feature: naming convention

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "azurerm_application_insights" {
   description = "The Azure Application Insights resource."
-  value       = azurerm_application_insights.insights
+  value       = azurerm_application_insights.application_insights
 }
