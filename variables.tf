572d6e3d5768be0d418cc93bc3b08cdd2a3e93e3
Lars Martin S <lmp@computas.com>
2022-11-25 13:08:43 +0100

    fix: system_short_name for å håndheve navnekonvensjon

--- variables.tf
+++ variables.tf
@@ -1,3 +1,19 @@
+variable "system_short_name" {
+  description = <<EOT
+  (Required) Short abbreviation (to-three letters) of the system name that this resource belongs to (see naming convention guidelines).
+  Will be part of the final name of the deployed resource.
+  EOT
+  type        = string
+}
+
+variable "app_name" {
+  description = <<EOT
+  (Required) Name of this resource within the system it belongs to (see naming convention guidelines).
+  Will be part of the final name of the deployed resource.
+  EOT
+  type        = string
+}
+
 variable "environment" {
   description = "(Required) The name of the environment."
   type        = string
@@ -16,11 +32,6 @@ variable "environment" {
 #   description = "(Required) The name which should be used for this resource. Changing this forces a new resource to be created."
 #   type        = string
 # }
-
-variable "system_name" {
-  description = "(Required) The systen name which should be used for this resource. Changing this forces a new resource to be created."
-  type        = string
-}
 
 variable "override_name" {
   description = "(Optional) Override the name of the resource. Under normal circumstances, it should not be used."
