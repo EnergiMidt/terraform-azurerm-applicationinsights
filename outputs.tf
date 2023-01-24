f9b1939619b1c3548bfa03d3964e6721a1a35957
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2023-01-24 10:42:53 +0100

    feature: tc-392 application insights availability tests

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "azurerm_application_insights" {
   description = "The Azure Application Insights resource."
   value       = azurerm_application_insights.application_insights
 }
+
+output "azurerm_application_insights_web_test" {
+  description = "The Azure Application Insights Web Test resource."
+  value       = local.web_test
+}
