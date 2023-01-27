221dbcd04f786b077ae9f09eb5ed852830fee375
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2023-01-27 09:30:25 +0100

    fix: tc-392 tc-394 application insights web test / monitor action group

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ resource "azurerm_monitor_action_group" "action_group" {
   short_name          = each.value.short_name
 
   dynamic "arm_role_receiver" {
-    for_each = (each.value.arm_role_receiver == null) ? [] : toset(each.value.arm_role_receiver)
+    for_each = (each.value.arm_role_receiver == null) ? [] : each.value.arm_role_receiver
 
     content {
       name                    = arm_role_receiver.value.name
